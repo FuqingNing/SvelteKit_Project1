9230210bea8766e2bf4925bb378d207f9daeca38
FuqingNing <87643230+FuqingNing@users.noreply.github.com>
2023-11-27 23:10:09 +1100

    toggleflag_extract

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,4 +1,4 @@
-export function initializeBoard(rows: number, columns: number,board: Array<Array<{ hasMine: boolean; status: string }>>,mineCount: number) {
+export function initializeBoard(board: Array<Array<{ hasMine: boolean; status: string }>>,rows: number, columns: number,mineCount: number) {
     for (let y = 0; y < rows; y++) {
         board[y] = [];
         for (let x = 0; x < columns; x++) {
@@ -35,4 +35,32 @@ export function checkWin(board: Array<Array<{ hasMine: boolean; status: string }
     }
 
     return allNonMinesRevealed;
+ }
+export function getAdjacentMines(board: Array<Array<{ hasMine: boolean; status: string }>>, x: number, y: number, rows: number, columns: number) {
+    let count = 0;
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+        for (let xOffset = -1; xOffset <= 1; xOffset++) {
+            if (xOffset === 0 && yOffset === 0) continue;
+            const newX = x + xOffset;
+            const newY = y + yOffset;
+            if (newX >= 0 && newX < columns && newY >= 0 && newY < rows) {
+                if (board[newY][newX].hasMine) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+export function toggleFlag(board: Array<Array<{ hasMine: boolean; status: string }>>, x: number, y: number, currentScore: { update: (fn: (n: number) => number) => void }) {
+    // alert('toggleFlag');
+    if (board[y][x].status === 'closed') {
+        board[y][x].status = 'flagged'; // if cell closed ,flag it
+        currentScore.update((n) => n - 5);
+    } else if (board[y][x].status === 'flagged') {
+        board[y][x].status = 'closed'; // unflag
+        currentScore.update((n) => n + 1); // update score
+    }
+    // checkWin();
+    checkWin(board, y, x);
 }
